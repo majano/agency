8e32faaf92bd06a033d7b43ce30c4fb08a5416ea
JM <juanjijo@gmail.com>
2020-06-06 14:06:13 -0600

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,38 +1,80 @@
 $(document).ready(function() {
 
-  // Scrolling header
-  $(window).scroll(function() {
 
+  // Scrolling header 
+  function scrollRender() {
     var header = $('.header');
-    var scroll = $(window).scrollTop();
+    var scroll = $(window).scrollTop();    
+    var navLink = $('.nav a');
 
     if(scroll > 100) {
       header.css('box-shadow','0 3px 12px rgba(0,0,0,0.1)');
+      navLink.removeClass('active');
     } else {
       header.css('box-shadow','none');
-    }    
+    }      
+
+    if(scroll >= 100) {
+      $('.section').each(function(i) {
+        if($(this).position().top <= scroll + 80) {
+          navLink.removeClass('active');
+          navLink.eq(i).addClass('active');
+        }
+      });
+    } else {
+      navLink.removeClass('active');
+      navLink.filter(':first').addClass('active');
+    }
+  }
+
+  scrollRender();
+
+  $(window).scroll(function() {
+
+    scrollRender();
   });
 
   // Anchor sections
   $('.nav a').on('click', function(e) {
     e.preventDefault();
-    
-    $('.nav a').removeClass('active');
-    $(this).addClass('active');
 
-    $('html,body').animate({
-      scrollTop: $($.attr(this,'href')).offset().top - 80
+    var anchorLink = $(this).attr('href');
+    var scrollPoint = $(anchorLink).offset().top - 70;
+
+    $('body,html').animate({
+        scrollTop: scrollPoint
     }, 800);
-  });
+
+    return false;
+  }); 
 
   // Keep in touch
   $('.cta_btn a, .header_wrap .btn').on('click', function(e) {
     e.preventDefault();
-    $('.overlay').fadeIn(500);
+    $('.overlay, .contactus').fadeIn(500);
   });
 
   $('.modal .close').on('click', function(e) {
     e.preventDefault();
-    $('.overlay').fadeOut();
+    $('.overlay, .contactus').fadeOut();
   });
+
+  // Work
+  $('.work_content img').each(function() {
+
+    var title = $(this).data('title');
+    var description = $(this).data('desc');
+    var imgURL = $(this).attr('src');
+    var modal = $('.project_profile');
+
+    $(this).on('click', function(){      
+  
+      $('.overlay, .project_profile').fadeIn(500);
+  
+      modal.find('img').attr('src', imgURL);
+      modal.find('h3').text(title);
+      modal.find('p').text(description);
+    }); 
+  });
+
 });
